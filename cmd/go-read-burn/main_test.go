6076c4c591d18bfdf44a2d26633f745cb8e91150
Dan Anstis <dan@bsod.co.nz>
2023-07-22 12:02:30 +1000

    fix: update test to use os rather than ioutil

--- cmd/go-read-burn/main_test.go
+++ cmd/go-read-burn/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -12,7 +11,7 @@ import (
 
 func TestCreateDBDir(t *testing.T) {
 	// Create a temporary directory for testing
-	tempDir, err := ioutil.TempDir("", "test")
+	tempDir, err := os.MkdirTemp("", "test")
 	if err != nil {
 		t.Fatalf("Failed to create temp directory: %v", err)
 	}
