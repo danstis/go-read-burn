e208b2e62e1182c6550f180cdb76e74b27795282
Dan Anstis <dan@bsod.co.nz>
2023-07-22 12:53:15 +1000

    fix: unexpected var, use let or const instead
    Fixes #77

--- cmd/go-read-burn/static/js/messageCount.js
+++ cmd/go-read-burn/static/js/messageCount.js
@@ -1,9 +1,8 @@
-var text_max = 8000;
+const text_max = 8000;
 $("#input_count").html("Password or secret (0 / " + text_max + " chars):");
 
 $("#inputText").keyup(function () {
-  var text_length = $("#inputText").val().length;
-  var text_remaining = text_max - text_length;
+  let text_length = $("#inputText").val().length;
 
   $("#input_count").html(
     "Password or secret (" + text_length + " / " + text_max + " chars):"
